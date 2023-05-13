848ed473f8febc93f86d43591de4a2f02deb9c1f
Ittisafur <ittisafur@gmail.com>
2023-05-14 01:19:59 +0600

    Wrap Up

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { createUserSchema } from "./schema/user.schema";
 import { createSessionHandler, deleteSessionHandler, getUserSessionHandler } from "./controller/session.controller";
 import { createSessionSchema } from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schema/product.schema";
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from "./controller/product.controller";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -16,6 +18,11 @@ function routes(app: Express) {
   );
   app.get("/api/sessions", requireUser, getUserSessionHandler);
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+  app.post('/api/products', [requireUser, validateResource(createProductSchema)], createProductHandler);
+  app.put('/api/products', [requireUser, validateResource(updateProductSchema)], updateProductHandler);
+  app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler);
+  app.delete('/api/products/:productId', [requireUser, validateResource(deleteProductSchema)], deleteProductHandler);
 }
 
 export default routes;
